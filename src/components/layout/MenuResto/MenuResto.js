963e1996d9ab45d0e9fcf194d2ea87c91e43d14a
aztran <andra.riztyan@gmail.com>
2019-03-01 18:49:16 +0700

    :sparkles: add components (baseTable, menu)

---
+++ src/components/layout/MenuResto/MenuResto.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Menu } from 'antd';
+
+const MenuResto = () => (
+  <Menu
+    theme="dark"
+    mode="horizontal"
+    defaultSelectedKeys={['1']}
+    style={{ lineHeight: '64px' }}
+  >
+    <Menu.Item key="1">
+      Home
+    </Menu.Item>
+    <Menu.Item key="2">
+      List Restaurants
+      <Link to="/list-restaurant" />
+    </Menu.Item>
+    <Menu.Item key="3">
+      Add New Restaurant
+    <Link to="/input-restaurant" />
+    </Menu.Item>
+  </Menu>
+)
+
+export default MenuResto;
