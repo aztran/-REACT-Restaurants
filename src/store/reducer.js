21c8746ff6656cc0074aa9b941fc6e628302225c
aztran <andra.riztyan@gmail.com>
2019-03-02 13:38:41 +0700

    :wrench: add case for reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -3,6 +3,10 @@
 const initialState = {
   restaurants: [],
   status: '',
+  getResto: [],
+  name: '',
+  description: '',
+  id: '',
 }
 
 export default function reducer (state = initialState, action) {
@@ -13,6 +17,18 @@ export default function reducer (state = initialState, action) {
       return {...state, status: 'error'}
     case 'ADD_RESTO':
       return {...state, status: 'success'}
+    case 'UPDATE_RESTO':
+      return {...state, status: 'success'}
+    case 'DELETE_RESTO':
+      return {...state, status: 'success'}
+    case 'FETCH_ONE':
+      // const data = state.getResto.push(action.payload);
+      // data.push(action.payload);
+      // console.log(action.name);
+      return {
+          ...state, 
+          status: 'success', getResto: action.payload, name: action.name, description: action.description, id: action.id
+      }
     default:
       return state
   }
