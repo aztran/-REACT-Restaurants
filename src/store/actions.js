a43ff61dcecfe287d49edba9d15e73ebcf58b166
aztran <andra.riztyan@gmail.com>
2019-03-02 13:38:18 +0700

    :wrench: add action for delete, fetch, and update

--- src/store/actions.js
+++ src/store/actions.js
@@ -37,3 +37,53 @@ export const fetchResto = () => {
     })
   }
 }
+
+
+export const deleteResto = (id) => {
+  return dispatch => {
+    axios.delete('/data/' + id + '.json')
+      .then(response => {
+        console.log(response);
+        dispatch({
+          type: 'DELETE_RESTO',
+          payload: response.data
+        })
+      })
+  }
+}
+
+export const FetchOne = (id) => {
+  return dispatch => {
+    axios.get('/data/' + id + '.json')
+      .then(res => {
+        // console.log(response);
+        //console.log(res.data);
+        let post = res.data;
+        post['id'] = id;
+        console.log(post);
+        // console.log(id);
+        dispatch({
+          type: 'FETCH_ONE',
+          id: post.id,
+          name: post.Name,
+          description: post.Description
+        })
+      })
+  }
+}
+
+export const updateResto = (id,name, description) => {
+  return dispatch => {
+    axios.put('/data/' + id + '.json',{
+      Name: name,
+      Description: description
+    })
+      .then(response => {
+        console.log(response);
+        // return;
+        dispatch({
+          type: 'UPDATE_RESTO',
+        })
+      })
+  }
+}
